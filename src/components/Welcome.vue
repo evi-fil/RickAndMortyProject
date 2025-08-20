3cb48802ee1f20be3cea4f1994316ba6e1769f76
evifil <203722943+evi-fil@users.noreply.github.com>
2025-08-20 20:35:40 +0300

    Update Welcome.vue

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,7 +3,7 @@
               bg-[url('/rick-and-morty-welcome-mobile-2.png')]
               md:bg-[url('/rick-and-morty-welcome-md.png')]
               lg:bg-[url('/rick-and-morty-welcome.png')]">
-		<div class="flex justify-center pt-120 md:pt-150">
+		<div class="flex justify-center pt-100 md:pt-150">
 			<RouterLink to="/all-characters">
 				<button class="mt-10 mb-20 bg-lime-400 text-fuchsia-800 
                  hover:ring hover:ring-lime-500 hover:shadow-md hover:shadow-lime-500
