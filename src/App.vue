035cca5e1bd21cd4c6ebc56c6b779e34cd6b676d
evifil <203722943+evi-fil@users.noreply.github.com>
2025-09-11 03:24:42 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="relative min-h-screen bg-center bg-repeat-y
-		bg-[url('rick-and-morty-mobile.jpg')]
-		bg-fixed md:bg-[url('/rick-and-morty.jpg')] 
-		md:bg-no-repeat md:bg-cover bg-blend-multiply bg-gray-400">
+		bg-[url('/rick-and-morty-mobile.jpg')]
+		md:bg-fixed md:bg-[url('/rick-and-morty.jpg')] 
+		md:bg-no-repeat md:bg-cover
+		bg-blend-multiply bg-gray-400">
 		<RouterView />
 	</div>
 </template>
